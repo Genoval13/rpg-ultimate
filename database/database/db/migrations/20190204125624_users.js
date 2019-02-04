eb72fc8d597fe4e7315b2422819ff554bdc6d00c
Bryan Anderson <bryan.texasbobcat@gmail.com>
2019-02-04 13:08:15 -0600

    added migrations

---
+++ database/database/db/migrations/20190204125624_users.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function (table) {
+    table.increments();
+    table.string('username');
+    table.string('password_hash');
+    table.string('token')
+    table.timestamps();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+};
